df0524a05c8ae11aa7284609709016904383a5b7
Manish kumar <manish@olivetech.net>
2023-07-20 16:03:13 +0530

    updated script

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,35 @@
 const path = require('path');
 const { NODE_ENV = 'development' } = process.env;
 const nodeExternals = require('webpack-node-externals');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin').TsconfigPathsPlugin;
 
 module.exports = {
-  entry: './server.ts',
+  entry: './server.ts', // Assuming your server.ts is in the 'src' folder
   mode: NODE_ENV,
   target: 'node',
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'server.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.js',
   },
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
+    plugins: [new TsconfigPathsPlugin()], // Allows resolving paths from tsconfig.json
   },
-  externals: [nodeExternals()],
+  externals: [nodeExternals()], // Exclude node_modules from the bundle
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: ['ts-loader']
-      }
-    ]
-  }
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true, // Faster builds in development mode
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
