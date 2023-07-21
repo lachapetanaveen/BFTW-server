de792724b85d9d35b31a63cc50be0877ad2e35ae
Manish kumar <manish@olivetech.net>
2023-07-21 09:49:18 +0530

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
-const { NODE_ENV = 'development' } = process.env;
+const { NODE_ENV } = process.env; // Don't set a default value, leave it undefined to be filled by dotenv-webpack
 const nodeExternals = require('webpack-node-externals');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin').TsconfigPathsPlugin;
+const Dotenv = require('dotenv-webpack'); // Import dotenv-webpack
 
 module.exports = {
   entry: './server.ts', // Assuming your server.ts is in the 'src' folder
-  mode: NODE_ENV,
+  mode: NODE_ENV || 'development', // Set 'development' as default if NODE_ENV is not defined
   target: 'node',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -32,4 +33,7 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new Dotenv(), // Use dotenv-webpack to load environment variables from .env file
+  ],
 };
